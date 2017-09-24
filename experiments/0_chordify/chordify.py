337484589fd11ecbc224c0284e5b6daa80d7a194
Yiu Man Chi <ymcatar@Air.ASUS-NETWORK>
2017-09-24 21:26:19 +0800

    add: chordify experiment

---
+++ experiments/0_chordify/chordify.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import music21
+import os
+
+xml_path = os.getcwd() + '/sample/'
+targetXml = '1b.xml'
+
+result = music21.converter.parse(xml_path + targetXml)
+
+chords = result.chordify()
+
+for c in chords.recurse().getElementsByClass('Chord'):
+    c.closedPosition(forceOctave=4, inPlace=True)
+
+prev = None
+for c in chords.recurse().getElementsByClass('Chord'):
+    rn = str(music21.roman.romanNumeralFromChord(c, music21.key.Key('C')).figure)
+    if rn != prev:
+        c.addLyric(rn)
+    prev = rn
+
+chords.show()
+
+# detect modulation
+for m in chords.getElementsByClass('Measure'):
+    k = m.analyze('key')
+    print(m.number, k)
