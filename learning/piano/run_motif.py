84a9fa24541b9a462c4edfde92f1b0dee77e505e
Yiu Man Chi <ymcatar@gmail.com>
2017-11-20 22:04:28 +0800

    add: print only option

--- learning/piano/run_motif.py
+++ learning/piano/run_motif.py
@@ -11,8 +11,8 @@ from algorithm.motif.algorithms import MotifAnalyzerAlgorithms
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="path of the input MusicXML file")
-parser.add_argument(
-    "-o", "--output", help="output folder path")
+parser.add_argument("-o", "--output", help="output folder path")
+parser.add_argument("-p", "--print-only", help="print the result only", action='store_true')
 
 args = parser.parse_args()
 
@@ -28,8 +28,9 @@ motifs = analyzer.get_top_motif_cluster(verbose=True)
 for notegram_group in motifs:
     analyzer.highlight_notegram_group(notegram_group, '#ff0000')
 
-if args.output:
-    analyzer.score.write('musicxml', os.path.join(
-        output_path, filename + '.xml'))
-else:
-    analyzer.score.show()
+if not args.print_only:
+    if args.output:
+        analyzer.score.write('musicxml', os.path.join(
+            output_path, filename + '.xml'))
+    else:
+        analyzer.score.show()
