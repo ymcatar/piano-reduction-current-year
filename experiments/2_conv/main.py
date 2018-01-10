98e16380e7e813a1171620e64caa61903fc5311a
ymcatar <ymcatar@gmail.com>
2018-01-10 12:33:19 +0800

    update: multiple voice support

--- experiments/2_conv/main.py
+++ experiments/2_conv/main.py
@@ -9,61 +9,68 @@ import scipy.misc
 import numpy as np
 import random
 
+# constants
+PIANO_PS_MIN = 21
+PIANO_PS_MAX = 108
+QUANTUM = 16
+NUM_MEASURES = 100
+MAX_NOTE_NUM_PER_PART = 5
+
 # color scale
 from matplotlib import cm, colors
-# piano pitch class range: 21 - 108
-m = cm.ScalarMappable(colors.Normalize(vmin=21, vmax=108), 'hsv')
-colors = m.to_rgba(range(88))
+m = cm.ScalarMappable(colors.Normalize(vmin=PIANO_PS_MIN, vmax=PIANO_PS_MAX), 'hot')
+colors = m.to_rgba(range(PIANO_PS_MAX - PIANO_PS_MIN + 1))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="path of the input MusicXML file")
-
+parser.add_argument("-o", "--output", help="output png path")
 args = parser.parse_args()
 
 score = music21.converter.parse(args.input)
-score = score.quantize()
-
-# for part in score.recurse().getElementsByClass('Part'):
-#     measures = list(part.getElementsByClass('Measure'))
-#     print(measures)
-
-QUANTUM = 16
-
-img = None
-
+score = score.stripTies()
 parts = list(i for i in score.recurse().getElementsByClass('Part'))
 
-for i in range(len(parts)):
-    part = parts[i]
+img = None
+filled = None
+i = 0
+
+for part in parts:
     measures = list(part.recurse().getElementsByClass(('Measure')))
+    measures_len = min(NUM_MEASURES, len(measures))
 
     # the first part
     if i == 0:
-        width = math.ceil(len(measures) * float(QUANTUM)) + len(measures)
-        height = len(parts) * 2
-        img = np.zeros((height, width, 3), dtype=np.uint8)
+        width = math.ceil(measures_len * float(QUANTUM)) + measures_len
+        height = len(parts) * MAX_NOTE_NUM_PER_PART
+        img = np.full((height, width, 3), 0)
+        filled = np.zeros((height, width), dtype=np.int)
 
     j = 0
 
-    for measure in measures:
+    for measure in measures[:measures_len]:
         duration = measure.duration.quarterLength
         offsetMap = measure.offsetMap()
         for note in offsetMap:
-            if note.voiceIndex is None and isinstance(note.element, music21.note.Note):
+            if isinstance(note.element, music21.note.Note):
                 lower = math.floor(note.offset * float(QUANTUM) / duration) # replace with time signature
                 upper = math.floor(note.endTime * float (QUANTUM) / duration) # replace with time signature
-
                 color = colors[math.floor(note.element.pitch.ps - 21.0)]
-
                 for k in range(lower, upper):
-                    img[i*2, j+k][0] = color[0] * 255
-                    img[i*2, j+k][1] = color[1] * 255
-                    img[i*2, j+k][2] = color[2] * 255
-    
-        j += QUANTUM + 1
+                    if filled[i, j+k] > MAX_NOTE_NUM_PER_PART:
+                        break
+                    print(filled[i, j+k])
+                    img[i * MAX_NOTE_NUM_PER_PART + filled[i, j+k], j+k][0] = color[0] * 255
+                    img[i * MAX_NOTE_NUM_PER_PART + filled[i, j+k], j+k][1] = color[1] * 255
+                    img[i * MAX_NOTE_NUM_PER_PART + filled[i, j+k], j+k][2] = color[2] * 255
+                    filled[i, j+k] += 1
+
+        j += QUANTUM
+
+    i += 1
 
 display_img = np.kron(img, np.ones((4,4,1)))
 
-# scipy.misc.imsave('after.png', display_img)
-scipy.misc.imshow(display_img)
-# score.plot('pianoroll')
+if not args.output:
+    scipy.misc.imshow(display_img)
+else:
+    scipy.misc.imsave(args.output, display_img)
