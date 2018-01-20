5443996cc23315c00750c8913bbc48c4aadd5b51
ymcatar <ymcatar@gmail.com>
2018-01-20 20:41:33 +0800

    improve cli output

--- learning/piano/algorithm/motif/notegram.py
+++ learning/piano/algorithm/motif/notegram.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-from .algorithms import MotifAnalyzerAlgorithms
+from .algorithms import MotifAnalyzerAlgorithms, preprocess_note_list, convert_chord_to_highest_note
 
+def nice_note_sequence_func(note_list):
+    results = []
+    for curr_note in note_list:
+        string = None 
+        if curr_note.isRest:
+            string = 'R'
+        else:
+            string = curr_note.name
+        if curr_note.tie is not None:
+            string += '~'
+        results.append(string)
+    return results
+
+def nice_rhythm_sequence_func(note_list):
+    results = []
+    for curr_note in note_list:
+        results.append('{0:.1f}'.format(float(curr_note.duration.quarterLength)))
+    return results
 
 class Notegram(object):
 
@@ -19,15 +37,18 @@ class Notegram(object):
         return id(self)
 
     def __str__(self):  # this determines how notegram are grouped together
+        note_list = self.note_list
+        note_list = preprocess_note_list(note_list)
         return ';'.join(i[0] + ',' + i[1] for i in zip(
-            MotifAnalyzerAlgorithms.note_sequence_func(self.note_list),
-            MotifAnalyzerAlgorithms.rhythm_sequence_func(self.note_list)
+            MotifAnalyzerAlgorithms.note_sequence_func(note_list),
+            MotifAnalyzerAlgorithms.rhythm_sequence_func(note_list)
         ))
 
     def to_nice_string(self):
-        return '; '.join(i[0].rjust(2) + ', ' + i[1] for i in zip(
-            MotifAnalyzerAlgorithms.note_sequence_func(self.note_list),
-            MotifAnalyzerAlgorithms.original_rhythm_sequence_func(self.note_list)
+        note_list = convert_chord_to_highest_note(self.note_list)
+        return '; '.join(i[0].rjust(3) + ', ' + i[1] for i in zip(
+            nice_note_sequence_func(note_list),
+            nice_rhythm_sequence_func(note_list)
         ))
 
     def get_note_list(self):
