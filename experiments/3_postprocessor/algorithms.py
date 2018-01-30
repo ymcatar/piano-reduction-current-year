650aa8590b015060e1d8371beb1c44d1ca18f1e0
ymcatar <ymcatar@gmail.com>
2018-01-31 00:06:46 +0800

    refactor

--- experiments/3_postprocessor/algorithms.py
+++ experiments/3_postprocessor/algorithms.py
@@ -4,41 +4,28 @@ from util import isNote, isChord, highlight
 
 # Constants
 MAX_CONCURRENT_NOTE = 4
+MAX_PITCH_STEP_CHANGE = 8
 
 class PostProcessorAlgorithms(object):
 
     @staticmethod
-    def concurrent_notes(group):
+    def concurrent_notes(group_tuple):
+        offset, group = group_tuple
+        num_of_notes = 0
 
-        merged_offset_map = defaultdict(lambda: [])
-        sites = []
+        # determine the number of notes playing at each time instance
+        for noteOrChord in group:
+            if isNote(noteOrChord):
+                num_of_notes += 1
+            elif isChord(noteOrChord):
+                num_of_notes += noteOrChord.chordTablesAddress.cardinality
 
-        for measure in group:
-            offset_map = measure.offsetMap()
-            for item in offset_map:
-                merged_offset_map[item.offset].append(item.element)
+        # highlight all time instance with note playing >= MAX_CONCURRENT_NOTE
+        if num_of_notes >= MAX_CONCURRENT_NOTE:
+            for noteOrChord in group:
+                highlight(noteOrChord, 'red')
 
-            # determine the number of notes playing at each time instance
-            num_of_notes_at_offset = defaultdict(lambda: 0)
-            for offset, notes in merged_offset_map.items():
-                for noteOrChord in notes:
-                    if isNote(noteOrChord):
-                        num_of_notes_at_offset[offset] += 1
-                    elif isChord(noteOrChord):
-                        num_of_notes_at_offset[offset] += noteOrChord.chordTablesAddress.cardinality
-
-            # highlight all time instance with note playing >= MAX_CONCURRENT_NOTE
-            for offset, count in num_of_notes_at_offset.items():
-                if count >= MAX_CONCURRENT_NOTE:
-                    sites.append(merged_offset_map[offset])
-                    for noteOrChord in merged_offset_map[offset]:
-                        highlight(noteOrChord, 'red')
-                    # attempt to fix
-                    # ...
-                else:
-                    del merged_offset_map[offset]
-
-        return sites
+        return group
 
     @staticmethod
     # FIXME: probably a bad fix musically, refine later
@@ -71,3 +58,33 @@ class PostProcessorAlgorithms(object):
                 highlight(items.pop(0), 'blue')
                 note_num -= 1
 
+    @staticmethod
+    # FIXME: need some sort of "finger assignment"
+    def large_hand_movement(measure):
+        sites = []
+        notes = list(measure.recurse().getElementsByClass(('Rest', 'Note', 'Chord')))
+        note_pairs = list((notes[i], notes[i+1]) for i, _ in enumerate(notes[:-1]))
+        for (first, last) in note_pairs:
+            first_notes = first._notes if isChord(first) else [first] if isNote(first) else []
+            last_notes = last._notes if isChord(last) else [last] if isNote(last) else []
+            min_pair = None
+            min_val = None
+            for x in first_notes:
+                for y in last_notes:
+                    if min_val is None or abs(x.pitch.ps - y.pitch.ps) < min_val:
+                        min_pair = [x, y]
+                        min_val = abs(x.pitch.ps - y.pitch.ps)
+
+        if min_val is not None and min_val >= MAX_PITCH_STEP_CHANGE:
+            sites.append(min_pair)
+
+        for pair in sites:
+            first, last = pair
+            highlight(first, '#00ff00')
+            highlight(last, '#00ff00')
+
+        return sites
+
+    @staticmethod
+    def fix_large_hand_movement(measure):
+        pass
--- experiments/3_postprocessor/post_processor.py
+++ experiments/3_postprocessor/post_processor.py
@@ -10,6 +10,13 @@ from algorithms import PostProcessorAlgorithms
 # algorithm set
 
 ALGORITHMS_BY_MEASURE_GROUP = [
+]
+
+ALGORITHMS_BY_MEASURE = [
+    (PostProcessorAlgorithms.large_hand_movement, PostProcessorAlgorithms.fix_large_hand_movement)
+]
+
+ALGORITHMS_BY_OFFSET_GROUPED_NOTES = [
     (PostProcessorAlgorithms.concurrent_notes, PostProcessorAlgorithms.fix_concurrent_notes)
 ]
 
@@ -17,21 +24,41 @@ class PostProcessor(object):
 
     def __init__(self, score):
 
+        self.score = score
+
         # get all measures from the score
-        self.measures = list(score.recurse().getElementsByClass('Measure'))
+        self.measures = list(self.score.recurse(skipSelf=True).getElementsByClass('Measure'))
+        for measure in self.measures:
+            measure.removeByClass(
+                [music21.layout.PageLayout, music21.layout.SystemLayout]
+            )
 
         # group measures with the same offset together
         self.grouped_measures = defaultdict(lambda: [])
         for measure in self.measures:
            self.grouped_measures[str(measure.offset)].append(measure)
 
+        # group notes starting at the same time instance together
+        self.offset_grouped_notes = defaultdict(lambda: [])
+        for _, group in self.grouped_measures.items():
+            for measure in group:
+                offset_map = measure.offsetMap()
+                for item in offset_map:
+                    self.offset_grouped_notes[measure.offset + item.offset].append(item.element)
+
         # dictionary to store all the problematic sites
         self.sites = defaultdict(lambda: [])
 
     def apply(self):
-        for _, group in self.grouped_measures.items():
-            for algorithms in ALGORITHMS_BY_MEASURE_GROUP:
-                detect_func, fix_func = algorithms
-                self.sites[detect_func.__name__] += detect_func(group)
-                for site in self.sites[detect_func.__name__]:
-                    fix_func(site)
+        self.applyEach(ALGORITHMS_BY_MEASURE, self.measures)
+        self.applyEach(ALGORITHMS_BY_MEASURE_GROUP, self.grouped_measures)
+        self.applyEach(ALGORITHMS_BY_OFFSET_GROUPED_NOTES, self.offset_grouped_notes)
+
+    def applyEach(self, algorithms, source):
+        source = source.items() if isinstance(source, dict) else source
+        for item in source:
+            for algorithm in algorithms:
+                detect_func, fix_func = algorithm
+                self.sites[detect_func.__name__] += detect_func(item)
+                # for site in self.sites[detect_func.__name__]:
+                    # fix_func(site)
