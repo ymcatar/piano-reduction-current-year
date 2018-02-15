dfe4916a62ae1eb2b3636c65f30185d632aafdc6
Jason Choi <jasonchoi.mtt@gmail.com>
2018-02-15 13:03:15 +0000

    pitch distance feature

---
+++ learning/piano/algorithm/pitch_distance.py
@@ -0,0 +1,13 @@
+from .base import ReductionAlgorithm, get_markings, iter_notes
+
+
+class PitchDistance(ReductionAlgorithm):
+    dtype = 'float'
+    range = (0.0, None)
+
+    def run(self, score_obj):
+        '''
+        The distance from middle C.
+        '''
+        for n in iter_notes(score_obj._score, recurse=True):
+            get_markings(n)[self.key] = abs(n.pitch.ps - 60)
