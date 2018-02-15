b337db342b6ae17c5c05100ac3240b0c467e2480
Jason Choi <jasonchoi.mtt@gmail.com>
2018-02-15 07:13:25 +0000

    fix: mscore detection in Linux

--- scoreboard/server.py
+++ scoreboard/server.py
@@ -6,6 +6,7 @@ import glob
 import logging
 import json
 import os
+import shutil
 import subprocess
 import sys
 import webbrowser
@@ -84,8 +85,10 @@ async def ensure_xml_render(path):
         try:
             logging.info('Rendering XML: {}'.format(path))
             mscore = environ_local['musescoreDirectPNGPath']
+            if not mscore:
+                mscore = shutil.which('mscore')
             if not mscore or not os.path.exists(mscore):
-                raise web.HTTPInternalServerError('MuseScore not installed')
+                raise web.HTTPInternalServerError(text='MuseScore not installed')
             process = await asyncio.create_subprocess_exec(
                 mscore, '-o', path[:-3] + 'svg', '-T', '0', path)
             await process.wait()
@@ -103,7 +106,7 @@ async def ensure_xml_render(path):
                     os.remove(out_path)
                     raise
         except subprocess.SubprocessError:
-            raise web.HTTPInternalServerError('Error rendering XML')
+            raise web.HTTPInternalServerError(text='Error rendering XML')
 
     return out_path
 
