87125dccc71ec2860befcc16149a66c6d85dc1db
ymcatar <ymcatar@gmail.com>
2018-02-26 18:07:10 +0800

    update: cli options

--- postprocessor/run.py
+++ postprocessor/run.py
@@ -8,7 +8,10 @@ from post_processor import PostProcessor
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="path of the input MusicXML file")
-parser.add_argument("-n", "--no-output", help="disable opening in Musescore after finish", action='store_true')
+parser.add_argument("-v", "--visualization", help="show the piano fingering visualization", action='store_true')
+parser.add_argument("-i", "--intermediate", help="display the intermediate score in musescore", action='store_true')
+parser.add_argument("-r", "--reduced", help="display the reduced score in musescore", action='store_true')
+
 args = parser.parse_args()
 
 score = music21.converter.parse(args.input)
@@ -16,10 +19,13 @@ score = music21.converter.parse(args.input)
 post_processor = PostProcessor(score)
 post_processor.apply()
 
-if not args.no_output:
-
-    # results = post_processor.generate_piano_score()
-    # results.show()
-
+if args.visualization:
     from visualizer import Visualizer
     results = Visualizer(post_processor.score)
+
+if args.intermediate:
+    post_processor.show()
+
+if args.reduced:
+    results = post_processor.generate_piano_score()
+    results.show()
