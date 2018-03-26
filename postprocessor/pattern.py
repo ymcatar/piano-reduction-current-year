b9aa66bc15ec06a9ae11f789f416ef87bfc1d73f
ymcatar <ymcatar@gmail.com>
2018-03-26 12:23:52 +0800

    fix: cluster number function

--- postprocessor/pattern.py
+++ postprocessor/pattern.py
@@ -5,34 +5,21 @@ import numpy as np
 from termcolor import colored
 from collections import defaultdict
 
+from util import print_vector
+
 class PatternAnalyzer(object):
 
     def __init__(self, piano_roll):
         self.piano_roll = piano_roll
-
-    def print_vector(self, i):
-
-        def value_to_block(value):
-            if value == 0:
-                return '▒'
-            elif value == 1:
-                return '█'
-            else:
-                return colored('█', 'red')
-
-        message = '{:s} {:s}'.format(
-            '{:2d}'.format(i).zfill(3), # leftpad literally cures cancer
-            ''.join(value_to_block(i) for i in self.piano_roll[i][12:])
-        )
-
-        print(message)
+        self.patterns = {}
 
     def print_piano_roll(self):
         for i, row in enumerate(piano_roll):
-            self.print_vector(i)
+            print_vector(row, i)
 
     def run(self):
-        self.detect_triads()
+        self.patterns['triads'] = self.detect_triads()
+        self.print_piano_roll()
 
     def detect_triads(self):
         for i, vector in enumerate(piano_roll):
--- postprocessor/util.py
+++ postprocessor/util.py
@@ -32,16 +32,27 @@ def construct_piano_roll(measures):
     return piano_roll
 
 def print_vector(vector, offset, notes=None, max_hand_span=7):
+
+    def value_to_block(value):
+        if value == 0:
+            return '▒'
+        elif value == 1:
+            return '█'
+        else:
+            return colored('█', 'blue')
+
     cluster_count = get_number_of_cluster(vector, max_hand_span)
     message = '{:s} {:s} ({:d})'.format(
         '{:.2f}'.format(offset).zfill(6),
-        ''.join('█' if i == 1 else '▒' for i in vector[12:]),
+        ''.join(value_to_block(i) for i in vector[12:]),
         cluster_count
     )
+
     if notes is not None:
         notes = [n for n in notes if not n.deleted]
         message += ' '
         message += ', '.join(n.note.pitch.name for n in notes)
+
     if cluster_count > 2:
         print(colored(message, 'red'))
     else:
@@ -50,8 +61,10 @@ def print_vector(vector, offset, notes=None, max_hand_span=7):
 def get_number_of_cluster(vector, max_hand_span):
 
     max_cluster_size = 2 * max_hand_span - 1
-    ps_list = [i for i, is_active in enumerate(vector) if is_active]
+    ps_list = [i for i, is_active in enumerate(vector) if int(is_active) == 1]
 
+    if len(ps_list) == 0: # no note => trivially no cluster
+        return 0
 
     if len(ps_list) == 1: # only one note => trivially one cluster
         return 1
@@ -62,7 +75,7 @@ def get_number_of_cluster(vector, max_hand_span):
     # greedily expand the left cluster until it is impossible
     for i, item in enumerate(ps_list):
         if item - ps_list[0] > max_cluster_size:
-            return 1 + get_number_of_cluster(vector[i:], max_hand_span)
+            return 1 + get_number_of_cluster(vector[item:], max_hand_span)
 
 def get_number_of_cluster_from_notes(notes, max_hand_span):
     vector = construct_vector(notes)
